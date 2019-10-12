4c629b00834e271349b681c4b2f1bfa3f552a990
Rui Lopes <rgl@ruilopes.com>
2019-10-12 14:29:33 +0100

    add support for caching https apt repositories

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,8 +4,22 @@ set -eux
 #
 # configure APT to use our cache APT proxy.
 # see http://10.10.10.3:3142
+# NB we cannot use APT::Update::Pre-Invoke because that is invoked after sources.list is
+#    loaded, so we had to override the apt-get command with our own version.
 
-echo 'Acquire::http::Proxy "http://10.10.10.3:3142";' >/etc/apt/apt.conf.d/00aptproxy
+apt_proxy_url='http://10.10.10.3:3142'
+echo "Acquire::http::Proxy \"$apt_proxy_url\";" >/etc/apt/apt.conf.d/00aptproxy
+cat >/usr/local/bin/apt-get <<EOF
+#!/bin/bash
+if [ "\$1" == 'update' ]; then
+    for p in \$(find /etc/apt/sources.list /etc/apt/sources.list.d -type f); do
+        sed -i -E 's,(deb(-src)? .*)https://,\1$apt_proxy_url/,g' \$p
+    done
+fi
+exec /usr/bin/apt-get "\$@"
+EOF
+chmod +x /usr/local/bin/apt-get
+hash -r
 
 
 #
@@ -24,3 +38,18 @@ apt-get upgrade -y
 apt-get install -y vim
 apt-get install -y golang
 apt-get install -y nginx
+
+
+#
+# install docker to see whether an https repository can be used (and our
+# /usr/local/bin/apt-get is working correctly).
+# see https://docs.docker.com/install/linux/docker-ce/ubuntu/
+# NB execute apt-cache madison docker-ce to known the available versions.
+docker_version="${1:-5:19.03.3~3-0~ubuntu-bionic}"; shift || true
+apt-get install -y apt-transport-https software-properties-common gnupg2
+wget -qO- https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y "docker-ce=$docker_version" "docker-ce-cli=$docker_version" containerd.io
+# let the vagrant user manage docker.
+usermod -aG docker vagrant
